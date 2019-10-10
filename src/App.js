03d182258dd2244082761bfd1cbbf9452bb3fd46
spirit-tv <tlitaudon@gmail.com>
2019-10-10 17:33:10 +0200

    first version

---
+++ src/App.js
@@ -0,0 +1,284 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <nav className="menu" tabindex="0">
+	<div className="smartphone-menu-trigger"></div>
+  <header className="avatar">
+		<img src="http://www.billselby.com/Graphics/PopUps/PopUps2/gamelogos/mastermind.jpg" />
+    <h2>Mastermind</h2>
+  </header>
+	<ul>
+    <li tabindex="0" className="icon-dashboard"><span>Games</span></li>
+    <li tabindex="0" className="icon-customers"><span>Score</span></li>
+    <li tabindex="0" className="icon-users"><span>Règle</span></li>
+    <li tabindex="0" className="icon-settings"><span>Settings en dev</span></li>
+  </ul>
+</nav>
+
+<main>
+  <div className="helper">
+          <h1>MasterMind GAMES</h1>
+          
+  </div>
+          <div className="top"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          <div className="d-flex justify-content-around">
+          <div className="plateau">
+          <div className="d-flex justify-content-around">
+            <div className="rond1"></div>
+            <div className="rond2"></div>
+            <div className="rond3"></div>
+            <div className="rond4"></div>
+          </div>
+          </div>
+          <div className="petit-plateau">
+          <div className="d-flex justify-content-around">
+            
+            <div className="petit-rond1"></div>
+            <div className="petit-rond2"></div>
+            </div>
+            <div className="petit-esp"></div>
+            <div className="d-flex justify-content-around">
+            <div className="petit-rond3"></div>
+            <div className="petit-rond4"></div>
+            </div>
+          </div>
+          </div>
+          <div className="ecar"></div>
+          
+          <div className="tableu">
+          <div className="d-flex justify-content-around">
+            
+            <div className="red"></div>
+            <div className="blue"></div>
+            <div className="yellow"></div>
+            <div className="green"></div>
+            <div className="purple"></div>
+            <div className="orange"></div>
+          </div>
+          <div className="petit-esp"></div>
+          <div class="d-flex justify-content-around">
+            
+          <button type="button" class="btn btn-outline-warning">Jouer</button>
+          <button type="button" class="btn btn-outline-warning">Rejouer</button>
+          </div>
+          
+          </div>
+</main>
+    </div>
+  );
+}
+
+export default App;
